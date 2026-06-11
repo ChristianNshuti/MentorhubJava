895ec7be3f62fbdb5be61427bf3968be181abaa0
christiannshuti <chrisnshuti943@gmail.com>
2026-06-11 07:34:31 +0200

    updated securityConfig.java

--- src/main/java/com/mentorhub/config/SecurityConfig.java
+++ src/main/java/com/mentorhub/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
